2da4b8c460d8f67d4d57c0162f7c784e904db025
SCA-IRCM <52911489+SCA-IRCM@users.noreply.github.com>
2019-12-10 12:57:23 +0100

    Update

--- README.md
+++ README.md
@@ -1,15 +1,6 @@
 # Demo
  R files and data set for a quick use of SingleCellSignalR
 
-The example dataset (example_dataset.txt) is made of 400 cells randomly chosen from the "8k PBMCs from a Healthy donor" dataset available at https://support.10xgenomics.com/single-cell-gene-expression/datasets/2.1.0/pbmc8k .
+The example dataset (example_dataset.txt) is made of 400 cells randomly chosen from the "8k PBMCs from a Healthy donor" dataset available at https://support.10xgenomics.com/single-cell-gene-expression/datasets/2.1.0/pbmc8k .    
 
----- 
-
-## Installation of Dependent Packages
-
-To install the package from the source file, download the install.R file and execute:
- 
-```
-source(install.R)
-install.packages("~/SingleCellSignalR_0.1.0.tar.gz")
-```
+A R script using Seurat (https://satijalab.org/seurat/) for preprocessing is also available (example_seurat.R)
--- example.R
+++ example.R
@@ -2,6 +2,7 @@ library(SingleCellSignalR)
 
 # Define your working directory
 setwd("~/example/")
+setwd("c:/Users/simon.cabello/Documents/GitHub/Demo/")
 
 # Define the file of interest you want to work with
 file = "example_dataset.txt"
@@ -17,28 +18,26 @@ tsne = clust$`t-SNE`
 
 # Cell classification
 my.markers = markers(c("immune"))
-class = cell_classifier(data=data, genes=rownames(data), markers = my.markers)
+class = cell_classifier(data=data, genes=genes, markers = my.markers)
 
 # Cluster analysis
-clust.ana = cluster_analysis(data = data, genes = rownames(data), cluster = cluster, markers = my.markers)
+clust.ana = cluster_analysis(data = data, genes = genes, cluster = cluster, markers = my.markers)
 
 # Proceed to cell signaling
 signal = cell_signaling(data = data, genes = genes, cluster = cluster,species = "homo sapiens")
 
 # Create internal and interface network
-net = networks(data = data, signal = signal, genes = genes, cluster = cluster)
-internal = net$internal_network
+inter.net = inter_network(data = data, signal = signal, genes = genes, cluster = cluster)
 
-# Calculate internal active pathways
-pw = interaction2pw(internal)
-
-# Show interactions invovled in a spedific pathway
+# Show interactions invovled in a specific pathway
 pathway = "Translocation of ZAP-70 to Immunological synapse"
 c.name = "cluster 3"
 int = pathway2interactions(pathway = pathway, c.name = c.name)
 
+intra = intra_network(goi = "ADCY9",data = data,genes = genes,cluster = cluster,coi="B-cells",signal=signal,plot = T,cell.prop = 0.2,c.names=c.names)
+
 # Visualization
-expression.plot(data = data, name = "CD14", tsne = tsne)
-expression.plot.2(data = data, name.1 = "CD40LG", name.2 = "CD40", tsne = tsne)
+expression.plot(data = data$complete.dataset, name = "CD14", tsne = tsne)
+expression.plot.2(data = data$complete.dataset, name.1 = "CD40LG", name.2 = "CD40", tsne = tsne)
 
 visualize(inter = signal, show.in = c(1),write.out = TRUE)
